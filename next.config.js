055cc447b536d65751d38b28fb00f72f63bf1bbf
kieferhfitch <49919203+kieferhfitch@users.noreply.github.com>
2025-01-28 15:52:27 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,11 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // Apply CORS headers to API routes
         source: '/api/:path*',
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
-            value: 'https://*.gohighlevel.com'
+            value: 'https://pathwayinsure.com'
           },
           {
             key: 'Access-Control-Allow-Methods',
@@ -17,6 +16,10 @@ const nextConfig = {
           {
             key: 'Access-Control-Allow-Headers',
             value: 'Content-Type, Authorization'
+          },
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true'
           }
         ]
       }
